e25ff4b2bc95d11711d263b57f0a7c743bf9312f
codestech <ryszardcichoszotawski@gmail.com>
2024-06-08 11:56:16 +0200

    feat: basic component serializing to nbt (without styling)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -48,6 +48,7 @@ sequenceOf(
   "text-serializer-legacy",
   "text-serializer-plain",
   "text-serializer-ansi",
+  "text-serializer-nbt"
 ).forEach {
   include("adventure-$it")
   project(":adventure-$it").projectDir = file(it)
