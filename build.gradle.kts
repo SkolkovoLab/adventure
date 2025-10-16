296bbeccfcb4765f9cb0ed156c1220ef1e88fb81
Spliterash <me@spliterash.ru>
2025-10-16 17:07:07 +0300

    Aboba

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 plugins {
   alias(libs.plugins.indra.sonatype)
   alias(libs.plugins.nexusPublish)
+  `maven-publish`
 }
-
-// Project metadata is configured in gradle.properties
+allprojects {
+  apply(plugin = "maven-publish")
+  publishing {
+    repositories {
+      maven {
+        name = "cherry"
+        url = uri("https://repo.cherry.pizza/repository/adventure-text-serializer-nbt/")
+        credentials {
+          username = "admin"
+          password = "bottom_text"
+        }
+      }
+    }
+  }
+}
